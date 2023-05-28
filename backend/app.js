47517b1966acb085d1ec4c21aec4ff4224a0f0da
Leonid Rublevskiy <leops@inbox.ru>
2023-05-28 12:56:12 +0300

    backPort===3000

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { errLog, reqLog } = require('./middlewares/logger');
 // =====================================================
 // Слушаем 3000 порт
 // const { PORT = 3000 } = process.env;
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const DATA_BASE = process.env.DATA_BASE || 'mongodb://localhost:27017/mestodb';
 
 const app = express();
